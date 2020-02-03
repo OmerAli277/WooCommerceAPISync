872fb4855fa380d1e990119070753cda903f01fb
hanifsarwary <hanifsarwari@addo.ai>
2020-02-04 00:34:24 +0500

    venv removed

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\H311812Z3R\PersonalProjects\Django\hyr-portal\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

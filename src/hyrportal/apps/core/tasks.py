d0c14753d1ac37e8a56d8a7e064e12f4022b4aab
hanifsarwary <hanifsarwari@addo.ai>
2020-02-17 00:37:02 +0500

    no import from hyrportal is working

--- src/hyrportal/apps/core/tasks.py
+++ src/hyrportal/apps/core/tasks.py
@@ -1,20 +1,21 @@
 from celery import Celery
 from datetime import timedelta
-from hyrportal.celery import app
-from .woo_task import woo_fn_sync
+# from woo_task import woo_fn_sync
 
+app = Celery()
+app.config_from_object('celeryconfig', namespace='CELERY')
 
 @app.task
 def see_you():
-    sync = woo_fn_sync()
+    # sync = woo_fn_sync()
     print("See you in ten seconds!")
     print('-----------------')
     print('Start syncing Customers')
-    sync.sync_customers()
+    # sync.sync_customers()
     print('Start syncing products')
-    sync.sync_products()
+    # sync.sync_products()
     print('Start syncing orders')
-    sync.sync_orders()
+    # sync.sync_orders()
 
 app.conf.beat_schedule = {
     "see-you-in-ten-seconds-task": {

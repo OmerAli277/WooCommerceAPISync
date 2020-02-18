3ee625a6b88f193150bb5070ffde0c827059463a
UsamaMalick <usamamalick22@gmail.com>
2020-02-18 23:29:14 +0500

    Settings UI updated

--- src/hyrportal/apps/core/views.py
+++ src/hyrportal/apps/core/views.py
@@ -249,93 +249,12 @@ class SettingsView(UpdateView):
 
     success_url = reverse_lazy('settings')
 
-    # success_url = reverse_lazy('settings')
-
-    # customer_page_num = 1
-    # """Parsing records for customers """
-    # while True:
-    #     current_dict = wcapi.get("customers", params={'per_page': 1, 'page': customer_page_num}).json()
-    #     for i in current_dict:
-    #             # print(i)
-    #             # obj = WooCustomer(customer_id=i['id'], first_name=i[0]['first_name'])
-    #             # obj.save()
-    #         try:
-    #             obj, created  = WooCustomer.objects.update_or_create(customer_id=i['id'], first_name=i['first_name'],
-    #                               last_name=i['last_name'], company=i['billing']['company'],
-    #                               address_1=i['billing']['address_1'],
-    #                               address_2=i['billing']['address_2'],
-    #                               city=i['billing']['city'], state=i['billing']['state'],
-    #                               postcode=i['billing']['postcode'],
-    #                               country=i['billing']['country'],
-    #                               email=i['email'], phone=i['billing']['phone'], date_created=i['date_created'],
-    #                               date_modified=i['date_modified'], is_paying_customer=i['is_paying_customer'])
-    #         except:
-    #             pass
-    #     if len(current_dict) > 0:
-    #         customer_page_num += 1
-    #     else:
-    #         break
-    #
-    # order_page_num = 1
-    #
-    # """Parsing records for orders """
-    #
-    # while True:
-    #     current_dict = wcapi.get("orders", params={'per_page': 1, 'page': order_page_num}).json()
-    #     try:
-    #         obj = WooOrder(order_id=current_dict[0]['id'], parent_id=current_dict[0]['parent_id'],
-    #                        number=current_dict[0]['number'], order_key=current_dict[0]['order_key'],
-    #                        created_via=current_dict[0]['created_via'],
-    #                        version=current_dict[0]['version'],
-    #                        status=current_dict[0]['status'], currency=current_dict[0]['currency'],
-    #                        discount_total=current_dict[0]['discount_total'],
-    #                        discount_tax=current_dict[0]['discount_tax'],
-    #                        shipping_total=current_dict[0]['shipping_total'],
-    #                        shipping_tax=current_dict[0]['shipping_tax'],
-    #                        cart_tax=current_dict[0]['cart_tax'], total=current_dict[0]['total'],
-    #                        total_tax=current_dict[0]['total_tax'],
-    #                        prices_include_tax=current_dict[0]['prices_include_tax'],
-    #                        payment_method=current_dict[0]['payment_method'],
-    #                        payment_method_title=current_dict[0]['payment_method_title'],
-    #                        transaction_id=current_dict[0]['transaction_id'],
-    #                        date_created=current_dict[0]['date_created'],
-    #                        date_modified=current_dict[0]['date_modified'],
-    #                        date_paid=current_dict[0]['date_paid'],
-    #                        date_completed=current_dict[0]['date_completed']
-    #                        )
-    #         obj.save()
-    #     except:
-    #         obj = WooOrder(order_id=current_dict[0]['id'], parent_id=current_dict[0]['parent_id'],
-    #                        number=current_dict[0]['number'], order_key=current_dict[0]['order_key'],
-    #                        created_via=current_dict[0]['created_via'],
-    #                        version=current_dict[0]['version'],
-    #                        status=current_dict[0]['status'], currency=current_dict[0]['currency'],
-    #                        discount_total=current_dict[0]['discount_total'],
-    #                        discount_tax=current_dict[0]['discount_tax'],
-    #                        shipping_total=current_dict[0]['shipping_total'],
-    #                        shipping_tax=current_dict[0]['shipping_tax'],
-    #                        cart_tax=current_dict[0]['cart_tax'], total=current_dict[0]['total'],
-    #                        total_tax=current_dict[0]['total_tax'],
-    #                        prices_include_tax=current_dict[0]['prices_include_tax'],
-    #                        payment_method=current_dict[0]['payment_method'],
-    #                        payment_method_title=current_dict[0]['payment_method_title'],
-    #                        transaction_id=current_dict[0]['transaction_id'],
-    #                        date_created=current_dict[0]['date_created'],
-    #                        date_modified=current_dict[0]['date_modified'],
-    #                        date_paid=current_dict[0]['date_paid'],
-    #                        date_completed=current_dict[0]['date_completed']
-    #                        )
-    #         obj.save()
-    #
-    #     if len(current_dict) > 0:
-    #
-    #         order_page_num += 1
-    #     else:
-    #         break
     def get_object(self, *args, **kwargs):
         return self.request.user
 
 @superuser_required()
+
+
 class UserListView(LoginRequiredMixin, TemplateView):
     template_name = 'user/list.html'
     model = User
@@ -391,13 +310,3 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
     model = User
     success_url = reverse_lazy('user-list')
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#
-#             return redirect('user/create.html')
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'registration/login.html', {form: form})

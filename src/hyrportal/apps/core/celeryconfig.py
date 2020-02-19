6fbfd5de85c2e5ac57a227b62f3caf69e66541dc
hanifsarwary <hanifsarwari@addo.ai>
2020-02-19 23:41:23 +0500

    celery configured with redis

--- src/hyrportal/apps/core/celeryconfig.py
+++
@@ -1,6 +0,0 @@
-CELERY_broker_url = 'pyamqp://guest@localhost//'
-CELERY_task_serializer = 'json'
-CELERY_result_serializer = 'json'
-CELERY_accept_content = ['json']
-CELERY_timezone = 'Europe/Oslo'
-CELERY_enable_utc = True
--- src/hyrportal/apps/core/tasks.py
+++ src/hyrportal/apps/core/tasks.py
@@ -1,17 +1,15 @@
-from celery import Celery
-from datetime import timedelta
-from hyrportal.apps.core.woo_task import woo_fn_sync
 
-app = Celery()
-app.config_from_object('celeryconfig', namespace='CELERY')
+from datetime import timedelta
+# from .woo_task import woo_fn_sync
+from hyrportal.celery import app
 
 @app.task
 def see_you():
-    sync = woo_fn_sync()
+    # sync = woo_fn_sync()
     print("See you in ten seconds!")
     print('-----------------')
     print('Start syncing Customers')
-    sync.sync_customers()
+    # sync.sync_customers()
     print('Start syncing products')
     # sync.sync_products()
     print('Start syncing orders')
@@ -19,7 +17,7 @@ def see_you():
 
 app.conf.beat_schedule = {
     "see-you-in-ten-seconds-task": {
-        "task": "core.see_you",
+        "task": "hyrportal.apps.core.tasks.see_you",
         "schedule": 10.0
     }
 }
